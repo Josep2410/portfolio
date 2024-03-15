888950e93c6d0f0ac95415b064d0cf9622b29be4
Josep2410 <josephpinoy2@yahoo.com>
2024-03-15 13:40:28 -0500

    data file

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+ const data = [
+  {
+    "title": "Pupuseria",
+    "img": "https://www.vvsupremo.com/foodservice/wp-content/uploads/2017/05/Pupusas-FS2-716x483-sRGB.jpg"
+  },
+  {
+    "title" : "We are the Champions",
+    "img" : "https://www.boredpanda.com/blog/wp-content/uploads/2016/11/queen-freddie-mercury-acapella-playback-fm-fb__700-png.jpg"
+  },
+  {
+    "title" : "Chrome Extension",
+    "img" : "https://www.stockvault.net/data/2011/05/31/124348/preview16.jpg"
+  },
+  {
+    "title" : "Javascript Documentation",
+    "img" : "https://govos.com/wp-content/uploads/2021/12/What-is-Records-Retention_550x375.jpg"
+  },
+]
+
+export default data
+
+
