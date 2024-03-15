b60b2d957985bb110bacdc38a7474908c73b3968
Josep2410 <josephpinoy2@yahoo.com>
2024-03-15 16:27:30 -0500

    added links to navigate app .

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
-    <h2>About</h2>
+    <>
+      <h2>About</h2>
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contacts</Link>
+      
+    </>
   )
 }
