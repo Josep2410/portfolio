bbfef57acabed604c3cc26a59defccb3b217c26b
Josep2410 <josephpinoy2@yahoo.com>
2024-03-15 13:39:41 -0500

    Added Projects page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import SingleProject from '../components/SingleProject'
+import '../styles/Projects.css'
+import data from '../data/data.js'
 
 export default function Projects() {
+  console.log(data)
   return (
-    <h2>Projects</h2>
+    <div className='projects-section'>
+      <h2>Projects</h2>
+      <main className='projects-container'>
+      {data.map(project => <SingleProject key={project.title} title={project.title} img={project.img}/>)}
+        
+      </main>
+    </div>
   )
 }
