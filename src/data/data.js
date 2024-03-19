d91b38f31c17a3ae721dc0da04224ee45aa4cf23
Josep2410 <josephpinoy2@yahoo.com>
2024-03-19 16:10:09 -0500

    add a data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,28 @@
  const data = [
   {
     "title": "Pupuseria",
-    "img": "https://www.vvsupremo.com/foodservice/wp-content/uploads/2017/05/Pupusas-FS2-716x483-sRGB.jpg"
+    "img": "https://www.vvsupremo.com/foodservice/wp-content/uploads/2017/05/Pupusas-FS2-716x483-sRGB.jpg",
+    "alt" : "a single pupusa with crisp edges and cabbage on top",
+    "link" : ""
   },
   {
     "title" : "We are the Champions",
-    "img" : "https://www.boredpanda.com/blog/wp-content/uploads/2016/11/queen-freddie-mercury-acapella-playback-fm-fb__700-png.jpg"
+    "img" : "https://www.boredpanda.com/blog/wp-content/uploads/2016/11/queen-freddie-mercury-acapella-playback-fm-fb__700-png.jpg",
+    "alt" : "Freddie Mercury singings on stage holding the microphone close to his face",
+    "link" : ""
   },
   {
     "title" : "Chrome Extension",
-    "img" : "https://www.stockvault.net/data/2011/05/31/124348/preview16.jpg"
+    "img" : "https://www.stockvault.net/data/2011/05/31/124348/preview16.jpg",
+    "alt" : "A walking path surrounded by plants and palm trees",
+    "link" : ""
   },
   {
     "title" : "Javascript Documentation",
-    "img" : "https://govos.com/wp-content/uploads/2021/12/What-is-Records-Retention_550x375.jpg"
+    "img" : "https://govos.com/wp-content/uploads/2021/12/What-is-Records-Retention_550x375.jpg",
+    "alt" : "a single stack of papers and envelops",
+    "link" : "https://master--javascript-technical-documentation-pr.netlify.app/"
+
   },
 ]
 
