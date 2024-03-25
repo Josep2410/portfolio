a6c921898ceb4c74941343e4da1901b2a6cb54e3
Josep2410 <josephpinoy2@yahoo.com>
2024-03-25 16:56:30 -0500

    add "id" key / value to data objects

--- src/data/data.js
+++ src/data/data.js
@@ -1,23 +1,27 @@
  const data = [
   {
+    "id" : "1",
     "title": "Pupuseria",
     "img": "https://www.vvsupremo.com/foodservice/wp-content/uploads/2017/05/Pupusas-FS2-716x483-sRGB.jpg",
     "alt" : "a single pupusa with crisp edges and cabbage on top",
     "link" : ""
   },
   {
+    "id" : "2",
     "title" : "We are the Champions",
     "img" : "https://www.boredpanda.com/blog/wp-content/uploads/2016/11/queen-freddie-mercury-acapella-playback-fm-fb__700-png.jpg",
     "alt" : "Freddie Mercury singings on stage holding the microphone close to his face",
     "link" : ""
   },
   {
+    "id" : "3",
     "title" : "Chrome Extension",
     "img" : "https://www.stockvault.net/data/2011/05/31/124348/preview16.jpg",
     "alt" : "A walking path surrounded by plants and palm trees",
     "link" : ""
   },
   {
+    "id" : "4",
     "title" : "Javascript Documentation",
     "img" : "https://govos.com/wp-content/uploads/2021/12/What-is-Records-Retention_550x375.jpg",
     "alt" : "a single stack of papers and envelops",
